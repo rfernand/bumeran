98f8bb1df7ba4366cf40fd9b95840009453c2464
Rodrigo Fernández <rfernand@4talent.cl>
2015-05-22 12:47:15 -0500

    Fixed publication creation
    	modified:   lib/bumeran.rb
    	modified:   spec/lib/bumeran_spec.rb

--- lib/bumeran.rb
+++ lib/bumeran.rb
@@ -37,6 +37,10 @@ class Bumeran
 
   @@areas = []
   @@subareas = {}
+  @@paises = []
+  @@zonas = {}
+  @@localidades = {}
+  @@plan_publicaciones= {}
 
   # Default way to setup Bumeran.
   def self.setup
@@ -154,7 +158,7 @@ class Bumeran
 
     #json[:preguntas]  # optional
     json[:preguntas][0][:simple][:texto] = "Pregunta de prueba"                   # optional
-    json[:preguntas] << {choice: {texto: "Pregunta de alternativas", indiceCorrecta: 2, opciones: ["respuesta 1", "respuesta 2", "respuesta 3", "respuesta 4"]}}      # optional
+    json[:preguntas] << {choice: {texto: "Pregunta de alternativas", indiceCorrecta: 2, opciones: [{opcion: "respuesta 1"}, {opcion: "respuesta 2"}, {opcion: "respuesta 3"}, {opcion: "respuesta 4"}]}}      # optional
 
     #json["postulantesDiscapacitados"] false, #optional
 
@@ -203,8 +207,6 @@ class Bumeran
     json[:requisitos][:genero][:excluyente] = false             # required
     json[:tipoTrabajoId] =  4                                   # required
 
-    binding.pry
-
     publish(json.to_json)
   end
 
@@ -228,9 +230,9 @@ class Bumeran
 
   def get_publication(publication_id)
     get_publish_path = "/v0/empresas/avisos/#{publication_id}"
-    response = self.class.put(get_publish_path, @@options)
+    response = self.class.get(get_publish_path, @@options)
 
-    return parse_response(response)
+    return parse_response_to_json(response)
   end
 
   def areas
@@ -256,7 +258,7 @@ class Bumeran
     areas_path = "/v0/empresas/comunes/areas" 
     response = self.class.get(areas_path, @@options)
 
-    @@areas = parse_response(response)
+    @@areas = parse_response_to_json(response)
   end
 
   def get_subareas_in(area_id)
@@ -264,7 +266,77 @@ class Bumeran
     subareas_path = "/v0/empresas/comunes/areas/#{area_id}/subAreas" 
     response = self.class.get(subareas_path, @@options)
 
-    parse_response(response)
+    parse_response_to_json(response)
+  end
+
+  def paises
+    if @@paises.empty?
+      get_paises
+    end
+    @@paises
+  end
+
+  def zonas
+    if @@zonas.empty?
+      paises.each do |pais|
+        pais["zonas"] = get_zonas_in(pais["id"])
+        @@zonas[pais["id"]] = pais["zonas"]
+      end
+    end
+    @@zonas
+  end
+
+  def localidades
+    if @@localidades.empty?
+      zonas.each do |zona|
+        zona["localidades"] = get_localidades_in(zona["id"])
+        @@localidades[zona["id"]] = zona["localidades"]
+      end
+    end
+    @@localidades
+  end
+
+  # Servicios generales asociados a datos de localización
+  def get_paises
+    initialize
+    paises_path = "/v0/empresas/locacion/paises" 
+    response = self.class.get(paises_path, @@options)
+
+    @@paises = parse_response_to_json(response)
+  end
+
+  def get_zonas_in(pais_id)
+    initialize
+    zonas_path = "/v0/empresas/locacion/paises/#{pais_id}/zonas" 
+    response = self.class.get(zonas_path, @@options)
+
+    parse_response_to_json(response)
+  end
+
+  def get_localidades_in(zona_id)
+    initialize
+    localidades_path = "/v0/empresas/locacion/zonas/#{zona_id}/localidades" 
+    response = self.class.get(localidades_path, @@options)
+
+    parse_response_to_json(response)
+  end
+
+  def plan_publicaciones 
+    if @@plan_publicaciones.empty?
+      paises.each do |pais|
+        pais["plan_publicaciones"] = get_plan_publicaciones_in(pais["id"])
+        @@plan_publicaciones[pais["id"]] = pais["plan_publicaciones"]
+      end
+    end
+    @@subareas
+  end
+
+  def get_plan_publicaciones_in(pais_id)
+    initialize
+    plan_publicaciones_path = "/v0/empresas/planPublicaciones/#{pais_id}"
+    response = self.class.get(plan_publicaciones_path, @@options)
+
+    return parse_response_to_json(response)
   end
 
   def get_denominaciones
@@ -272,7 +344,7 @@ class Bumeran
     denominaciones_path = "/v0/empresas/denominaciones"
     response = self.class.get(denominaciones_path, @@options)
 
-    return parse_response(response)
+    return parse_response_to_json(response)
   end
 
   def get_direcciones
@@ -280,7 +352,7 @@ class Bumeran
     direcciones_path = "/v0/empresas/direcciones"
     response = self.class.get(direcciones_path, @@options)
 
-    return parse_response(response)
+    return parse_response_to_json(response)
   end
 
   def frecuencias_pago
@@ -288,7 +360,7 @@ class Bumeran
     frecuencias_pago_path = "/v0/empresas/comunes/frecuenciasPago"
     response = self.class.get(frecuencias_pago_path, @@options)
 
-    return parse_response(response)
+    return parse_response_to_json(response)
   end
 
   def idiomas
@@ -296,7 +368,7 @@ class Bumeran
     idiomas_path = "/v0/empresas/comunes/idiomas"
     response = self.class.get(idiomas_path, @@options)
 
-    return parse_response(response)
+    return parse_response_to_json(response)
   end
 
   def industrias
@@ -304,7 +376,7 @@ class Bumeran
     industrias_path = "/v0/empresas/comunes/industrias"
     response = self.class.get(industrias_path, @@options)
 
-    return parse_response(response)
+    return parse_response_to_json(response)
   end
 
   def niveles_idiomas
@@ -312,7 +384,7 @@ class Bumeran
     niveles_idiomas_path = "/v0/empresas/comunes/nivelesIdiomas"
     response = self.class.get(niveles_idiomas_path, @@options)
 
-    return parse_response(response)
+    return parse_response_to_json(response)
   end
 
   def tipos_trabajo
@@ -320,7 +392,7 @@ class Bumeran
     tipos_trabajo_path = "/v0/empresas/comunes/tiposTrabajo"
     response = self.class.get(tipos_trabajo_path, @@options)
 
-    return parse_response(response)
+    return parse_response_to_json(response)
   end
 
   # Servicios de estudios de los postulantes
@@ -329,7 +401,7 @@ class Bumeran
     areas_estudio_path = "/v0/estudios/areasEstudio" 
     response = self.class.get(areas_estudio_path, @@options)
 
-    return parse_response(response)
+    return parse_response_to_json(response)
   end
 
   def estados_estudio
@@ -337,7 +409,7 @@ class Bumeran
     estados_estudio_path = "/v0/estudios/estadosEstudio" 
     response = self.class.get(estados_estudio_path, @@options)
 
-    return parse_response(response)
+    return parse_response_to_json(response)
   end
 
   def tipos_estudio
@@ -345,7 +417,7 @@ class Bumeran
     tipos_estudio_path = "/v0/estudios/tiposEstudio" 
     response = self.class.get(tipos_estudio_path, @@options)
 
-    return parse_response(response)
+    return parse_response_to_json(response)
   end
 
   def get_estudio(estudio_id)
@@ -353,7 +425,7 @@ class Bumeran
     estudio_path = "/v0/estudios/#{estudio_id}" 
     response = self.class.get(estudio_path, @@options)
 
-    return parse_response(response)
+    return parse_response_to_json(response)
   end
 
   # Servicios de la experiencia laboral de los postulantes
@@ -362,32 +434,7 @@ class Bumeran
     experiencia_laboral_path = "/v0/experienciaLaborales/#{experiencia_laboral_id}" 
     response = self.class.get(experiencia_laboral_path, @@options)
 
-    return parse_response(response)
-  end
-
-  # Servicios generales asociados a datos de localización
-  def paises
-    initialize
-    paises_path = "/v0/empresas/locacion/paises" 
-    response = self.class.get(paises_path, @@options)
-
-    return parse_response(response)
-  end
-
-  def zonas_in(pais_id)
-    initialize
-    zonas_path = "/v0/empresas/locacion/paises/#{pais_id}/zonas" 
-    response = self.class.get(zonas_path, @@options)
-
-    return parse_response(response)
-  end
-
-  def localidades_in(zona_id)
-    initialize
-    localidades_path = "/v0/empresas/locacion/zonas/#{zona_id}/localidades" 
-    response = self.class.get(localidades_path, @@options)
-
-    return parse_response(response)
+    return parse_response_to_json(response)
   end
 
   # Servicio de postulaciones a los avisos publicados por las empresas
@@ -396,7 +443,7 @@ class Bumeran
     postulaciones_path = "/v0/empresas/postulaciones/#{postulacion_id}" 
     response = self.class.get(postulaciones_path, @@options)
 
-    return parse_response(response)
+    return parse_response_to_json(response)
   end
 
   def discard_postulacion(postulacion_id)
@@ -404,7 +451,7 @@ class Bumeran
     discard_postulaciones_path = "/v0/empresas/postulaciones/#{postulacion_id}/descartar" 
     response = self.class.put(discard_postulaciones_path, @@options)
 
-    return parse_response(response)
+    return parse_response_to_json(response)
   end
 
   
@@ -425,7 +472,7 @@ class Bumeran
 
     response = self.class.post(login_path, query: {grant_type: grant_type, client_id: client_id, username: username, password: password})
 
-    if parse_response(response)
+    if parse_response_to_json(response)
       # "All good!"
       @@access_token = response["accessToken"]
       token_type   = response["tokenType"]
@@ -438,6 +485,23 @@ class Bumeran
 
   private
   def parse_response(response)
+    case response.code
+      when 200..201
+        # "All good!"
+        return response.body
+      when 401
+        raise "Error 401: Unauthorized. Check login info.\n #{response.body}"
+      when 403
+        raise "Error 403: Forbidden"
+      when 404
+        raise "Error 404 not found"
+      when 500...600
+        raise "ZOMG ERROR #{response.code}: #{response.body}"
+      else
+        raise "Error #{response.code}, unkown response: #{response.body}"
+    end
+  end
+  def parse_response_to_json(response)
     case response.code
       when 200..201
         # "All good!"
--- spec/lib/bumeran_spec.rb
+++ spec/lib/bumeran_spec.rb
@@ -9,23 +9,41 @@ describe Bumeran do
     bumeran.has_valid_access_token?.should be_true
   end
 
-  it 'can publish' do
+  it 'can publish', publish: true do
     bumeran = Bumeran.new
-    bumeran.test_publish.code.should == 201
+    result = bumeran.test_publish
+    pp result
+    result.to_i.should > 0
   end
 
-  it 'can get areas' do
+  it 'can get publication', publish: true do
     bumeran = Bumeran.new
+    publication_id = bumeran.test_publish.to_i
+    pp publication_id
+    #binding.pry
+    
+    publication_id.should > 0
+    publication = bumeran.get_publication(publication_id)
+    #binding.pry
+    pp publication
+  end
+  
+
+  it 'can get areas', getters: true do
+    bumeran = Bumeran.new
+    pp bumeran.areas
     bumeran.areas.count.should > 0
   end
 
-  it 'can get subareas' do
+  it 'can get subareas', getters: true do
     bumeran = Bumeran.new
+    pp bumeran.subareas
     bumeran.subareas.count.should > 0
   end
 
-  it 'can get frecuencias_pago' do
+  it 'can get frecuencias_pago', getters: true do
     bumeran = Bumeran.new
+    pp bumeran.frecuencias_pago
     bumeran.frecuencias_pago.count.should > 0
   end
 
